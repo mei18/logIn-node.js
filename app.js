179987c4bef916706017724b34ea9b53348b50ae
Melanie Liang <melly.strearshy@gmail.com>
2016-10-11 23:10:07 -0600

    fixed

--- app.js
+++ app.js
@@ -7,18 +7,18 @@ app.use(bodyParser.urlencoded());
 
 app.get('/', function (req, res) {
   res.sendfile("./index.html");
-
+  console.log("get");
 });
 
 app.post('/', function (req, res) {
-  var username=req.body.username; 
+  var username=req.body.username;
   var password=req.body.password;
-  res.send("The Username and password u posted :"+username+"Password: "+password);
-  console.log("Username:" + username);
-  console.log("Password:" + password);
+  res.send("The Username you posted:"+username+"and the Password: "+password);
+  console.log("Username is:" + username);
+  console.log("Password is:" + password);
 
 });
 
-app.listen(8080, function () {
-  console.log('On port 8080!');
+app.listen(8888, function () {
+  console.log('On port 8888!');
 });
